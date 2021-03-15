4d880f43ef3da707fde70b134ebb6d7f5b33b5bd
Matheus Ha <matheus.ha@gmail.com>
2021-03-15 09:31:31 -0300

    case 5 e titulo do prof

--- aula05/src/ha/matheus/Sistema.java
+++ aula05/src/ha/matheus/Sistema.java
@@ -40,13 +40,20 @@ public class Sistema {
                 System.out.println("Opecao realizada com sucesso!");
                 break;
             case 5:
-                System.out.println("Nome da conta: ");
-                String nome= scanner.next();
-                System.out.println("Valor da conta: ");
+                System.out.println("Nome da Conta:");
+                String nome = scanner.next();
+                System.out.println("Valor da conta:");
                 double valorDaConta = scanner.nextDouble();
-                System.out.println("juros da conta: ");
+                System.out.println("Juros da conta:");
                 double jurosDaConta = scanner.nextDouble();
-                System.out.println("Informe");
+                System.out.println("Informe a data de vencimento (ano-mes-dia)?");
+                String dataVencimentoTitulo = scanner.next();
+                Titulo titulo = new Titulo(valorDaConta,nome,dataVencimentoTitulo, jurosDaConta);
+                if(this.conta.sacar(titulo.totalParaPagar())){
+                    System.out.println("Operação Realizada com Sucesso!");
+                } else {
+                    System.out.println("Algo de errado aconteceu!");
+                }
             default:
                 System.out.println("Essa funcionalidade ainda nao foi implementada");
 
--- aula05/src/ha/matheus/Titulo.java
+++ aula05/src/ha/matheus/Titulo.java
@@ -1,23 +1,20 @@
 package ha.matheus;
 
 import java.time.LocalDate;
-
 import static java.time.temporal.ChronoUnit.DAYS;
-
 
 public class Titulo {
     private double valor;
     private String descricao;
-    //para guardar dados de data
+    //Para guardar dados de data
     private LocalDate dataDeVencimento;
-    private double JurosAoDia;
+    private double jurosAoDia;
 
-
-    public Titulo(double valor, String descricao, LocalDate dataDeVencimento, double jurosAoDia) {
+    public Titulo(double valor, String descricao, String dataDeVencimento, double jurosAoDia) {
         this.valor = valor;
         this.descricao = descricao;
-        this.dataDeVencimento = dataDeVencimento;
-        this.JurosAoDia = jurosAoDia;
+        this.dataDeVencimento = LocalDate.parse(dataDeVencimento);
+        this.jurosAoDia = jurosAoDia;
     }
 
     public double totalParaPagar(){
@@ -29,10 +26,12 @@ public class Titulo {
     }
 
     private double calcularJuros() {
-        return this.valor + this.valor*this.JurosAoDia * dataDeVencimento.until(LocalDate)
+        return this.valor + this.valor * this.jurosAoDia * dataDeVencimento.until(LocalDate.now(), DAYS);
     }
 
     private boolean estaAtrasada() {
         return LocalDate.now().isAfter(dataDeVencimento);
     }
+
+
 }
