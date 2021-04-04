43d9e9aede2955755f5ef8869386f5f58aa6da69
Matheus Ha <matheus.ha@gmail.com>
2021-04-04 19:13:06 -0300

    Transacoes
    Comeco da classe

--- Atividades/Atividade01/src/ha/matheus/Transacoes.java
+++ Atividades/Atividade01/src/ha/matheus/Transacoes.java
@@ -1,4 +1,12 @@
 package ha.matheus;
 
+import java.util.Random;
+
 public class Transacoes {
+
+    private static int getRandomNumberInRange(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
 }
